3561b214863ec281157f3f85bf5780925906b8b8
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-11 06:31:29 -0400

    refactor: name preflight skip proof type

--- src/preflight.rs
+++ src/preflight.rs
@@ -1,3 +1,6 @@
+type VerifiedSkips = std::sync::Arc<Vec<std::collections::HashSet<std::path::PathBuf>>>;
+type PreflightPlan = (PreflightResult, VerifiedSkips);
+
 include!("preflight_v2/part1.rs");
 include!("preflight_v2/part2.rs");
 include!("preflight_v2/part3.rs");
