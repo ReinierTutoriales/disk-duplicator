7feb8a064c9ec6f930c95de2fdab6894698f9b4a
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-13 12:11:40 -0400

    test: cover buffered QD2 policy

---
+++ dotnet/RepartoCopier.Core.Tests/StorageWritePolicyTests.cs
@@ -0,0 +1,91 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepartoCopier.Core;
+
+namespace RepartoCopier.Core.Tests;
+
+[TestClass]
+public sealed class StorageWritePolicyTests
+{
+    [TestMethod]
+    public void SataSsdLargeExclusiveWriteUsesQueueDepthTwo()
+    {
+        var device = Device("SATA", StorageMediaKind.SolidState, trim: true);
+
+        var qd = StorageWritePolicy.BufferedLargeWriteQueueDepth(
+            device,
+            schedulerMaxOutstandingIo: 2,
+            fileSize: 64L * 1024 * 1024,
+            dataLength: 16 * 1024 * 1024);
+
+        Assert.AreEqual(2, qd);
+    }
+
+    [TestMethod]
+    public void NvmeLargeExclusiveWriteUsesQueueDepthTwoNotFour()
+    {
+        var device = Device("NVMe", StorageMediaKind.SolidState, trim: true);
+
+        var qd = StorageWritePolicy.BufferedLargeWriteQueueDepth(
+            device,
+            schedulerMaxOutstandingIo: 4,
+            fileSize: 512L * 1024 * 1024,
+            dataLength: 16 * 1024 * 1024);
+
+        Assert.AreEqual(2, qd);
+    }
+
+    [TestMethod]
+    public void SharedPhysicalDiskStaysQueueDepthOne()
+    {
+        var device = Device("SATA", StorageMediaKind.SolidState, trim: true) with
+        {
+            SharesPhysicalDevice = true,
+        };
+
+        Assert.AreEqual(
+            1,
+            StorageWritePolicy.BufferedLargeWriteQueueDepth(
+                device,
+                2,
+                64L * 1024 * 1024,
+                16 * 1024 * 1024));
+    }
+
+    [TestMethod]
+    public void HddUsbNetworkAndSmallTailStayQueueDepthOne()
+    {
+        var hdd = Device("SATA", StorageMediaKind.Rotational, trim: false);
+        var usb = Device("USB", StorageMediaKind.SolidState, trim: true);
+        var network = Device("Network", StorageMediaKind.Unknown, trim: null) with
+        {
+            IsNetwork = true,
+        };
+        var sata = Device("SATA", StorageMediaKind.SolidState, trim: true);
+
+        Assert.AreEqual(1, StorageWritePolicy.BufferedLargeWriteQueueDepth(hdd, 2, 64L * 1024 * 1024, 16 * 1024 * 1024));
+        Assert.AreEqual(1, StorageWritePolicy.BufferedLargeWriteQueueDepth(usb, 2, 64L * 1024 * 1024, 16 * 1024 * 1024));
+        Assert.AreEqual(1, StorageWritePolicy.BufferedLargeWriteQueueDepth(network, 2, 64L * 1024 * 1024, 16 * 1024 * 1024));
+        Assert.AreEqual(1, StorageWritePolicy.BufferedLargeWriteQueueDepth(sata, 2, 64L * 1024 * 1024, 1024 * 1024));
+    }
+
+    private static StorageDeviceInfo Device(string bus, StorageMediaKind media, bool? trim) =>
+        new(
+            @"E:\\copy",
+            @"E:\\",
+            4,
+            1,
+            bus,
+            media,
+            false,
+            512,
+            4096,
+            true,
+            null,
+            false,
+            "NTFS",
+            DriveType.Fixed,
+            false,
+            true,
+            trim,
+            0);
+}
