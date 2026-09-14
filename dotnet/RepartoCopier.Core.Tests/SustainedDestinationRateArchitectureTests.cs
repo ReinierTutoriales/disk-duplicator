606ec9866525100424bea8f166b6cae2ed7d2eb0
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-14 16:59:39 -0400

    test: lock single sustained-rate implementation

--- dotnet/RepartoCopier.Core.Tests/SustainedDestinationRateArchitectureTests.cs
+++ dotnet/RepartoCopier.Core.Tests/SustainedDestinationRateArchitectureTests.cs
@@ -10,17 +10,30 @@ public sealed class SustainedDestinationRateArchitectureTests
     [TestMethod]
     public void GlobalAndPerDestinationRatesShareOneSlidingWindowPrimitive()
     {
-        var telemetryFieldTypes = typeof(CopyTelemetry)
-            .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+        var telemetryFields = typeof(CopyTelemetry)
+            .GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+        var telemetryFieldTypes = telemetryFields
             .Select(field => field.FieldType)
             .ToArray();
         CollectionAssert.Contains(telemetryFieldTypes, typeof(SlidingByteRateWindow));
+        Assert.AreEqual(1, telemetryFieldTypes.Count(type => type == typeof(SlidingByteRateWindow)));
+
+        var legacyTelemetryFields = telemetryFields.Select(field => field.Name).ToArray();
+        CollectionAssert.DoesNotContain(legacyTelemetryFields, "_rateGate");
+        CollectionAssert.DoesNotContain(legacyTelemetryFields, "_writeSamples");
 
         var progressFieldTypes = typeof(DestinationProgress)
             .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
             .Select(field => field.FieldType)
             .ToArray();
         CollectionAssert.Contains(progressFieldTypes, typeof(SlidingByteRateWindow));
+        Assert.AreEqual(1, progressFieldTypes.Count(type => type == typeof(SlidingByteRateWindow)));
+
+        var progressMethods = typeof(DestinationProgress)
+            .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+            .Where(method => method.Name == "SustainedWriteRateSnapshot")
+            .ToArray();
+        Assert.AreEqual(2, progressMethods.Length);
 
         var snapshotProperties = typeof(DestinationSnapshot)
             .GetProperties(BindingFlags.Instance | BindingFlags.Public)
